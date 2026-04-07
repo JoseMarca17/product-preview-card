b1bb06f4742676cb498fb3ad9c6a151496de004e
Jose Marca <josemarca1374@gmail.com>
2026-04-07 09:13:31 -0400

    Image with scale

--- componets/ButtonSection.tsx
+++ componets/ButtonSection.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
+import iconCart from  '@/public/icon-cart.svg'
+import '@/componets/ButtonSection.css'
 
 const ButtonSection = () => {
   return (
-    <div>ButtonSection</div>
+    <div className="button-cart">
+      <Image width={17} src={iconCart} alt={'Icono del carrito'} />
+      <span className="letras-cart">Add to Cart</span>
+    </div>
   )
 }
 
--- componets/ProductCard.tsx
+++ componets/ProductCard.tsx
@@ -1,18 +1,30 @@
-
 import Image from 'next/image'
+import {getImageProps} from 'next/image'
 import Detalles from './Detalles'
 import SeccionPrecios from './SeccionPrecios'
 import '@/componets/ProductCard.css'
+import ButtonSection from './ButtonSection'
 
 import imagen from '@/public/image-product-mobile.jpg'
+import imagenDesktop from '@/public/image-product-desktop.jpg'
+
 
 const ProductCard = () => {
+    const common = {alt: 'Imagen del producto', fill: true}
+    const {props: {srcSet: desktop}} = getImageProps({...common, src: imagenDesktop});
+    const {props: {srcSet: mobile, ...rest}} = getImageProps({...common, src: imagen});
     return (
         <div className="product-card">
-            <Image width={350} src={imagen} alt={'imagen del producto'} />
-            <Detalles />
-            <SeccionPrecios />
-
+            <picture className='image-desktop'>
+                <source media="(max-width: 1023px)" srcSet={mobile} />
+                <source media="(min-width: 1024px)" srcSet={desktop} />
+                <img {...rest} style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+            </picture>
+            <div className="content-container">
+                <Detalles />
+                <SeccionPrecios />
+                <ButtonSection />
+            </div>
         </div>
     )
 }
